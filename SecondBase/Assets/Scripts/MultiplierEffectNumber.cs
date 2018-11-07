839bca624489d268071400df0dfb1ed8110c4b11
Arthur Uzulin <fuj1nman@hotmail.com>
2018-11-07 16:01:55 +1100

    Added multiplier floataway

---
+++ SecondBase/Assets/Scripts/MultiplierEffectNumber.cs
@@ -0,0 +1,22 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class MultiplierEffectNumber : MonoBehaviour
+{
+    public float speed = 2.5F;
+
+    private Vector2 direction;
+
+    private void Start()
+    {
+        TextMeshPro tmp = GetComponent<TextMeshPro>();
+        tmp.text = string.Format(tmp.text, GameController.Instance.Multiplier);
+
+        direction = new Vector2(Random.Range(-1F, 1F), Random.Range(-1F, 1F));
+    }
+
+    private void Update()
+    {
+        transform.position += (Vector3)direction * speed * Time.deltaTime;
+    }
+}
